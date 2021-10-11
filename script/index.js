225acc329c234322cf6bdd32bb3c46da1cb7fb15
alvinlouis29 <alvinlouis29@gmail.com>
2021-10-12 02:17:43 +0300

    Add index.js and target the close button

---
+++ script/index.js
@@ -0,0 +1,6 @@
+let closeButton = document.getElementById('close');
+
+ closeButton.onclick = function () {
+     let mobile_Menu = document.getElementById('mobileMenu');
+     mobile_Menu.style.left = '-100%';
+ }
