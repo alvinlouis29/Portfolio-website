a61b9aad9cd4c4244ade0ba7cdaba331d14f2d89
alvinlouis29 <alvinlouis29@gmail.com>
2021-10-13 16:59:29 +0300

    Create popup javascript

--- script/index.js
+++ script/index.js
@@ -2,6 +2,9 @@ const closeButton = document.getElementById('close');
 const humbugerMenu = document.getElementById('humbuger');
 const mobileMenu = document.getElementById('mobileMenu');
 const menuItems = document.getElementsByClassName('menu-item');
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
 
 function hide() {
   mobileMenu.classList.toggle('menu-out');
@@ -12,4 +15,37 @@ humbugerMenu.onclick = hide;
 
 for (let i = 0; i < menuItems.length; i += 1) {
   menuItems[i].onclick = hide;
+}
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
 }
